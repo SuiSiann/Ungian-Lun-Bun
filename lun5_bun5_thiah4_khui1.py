f225e8cdfb0c513463bb70819f4efc0c95d06c47
a8568730 <a8568730@hotmail.com>
2018-09-06 17:45:23 +0800

    連線請求羅馬字

--- lun5_bun5_thiah4_khui1.py
+++ lun5_bun5_thiah4_khui1.py
@@ -1,5 +1,8 @@
 import sys
 import re
+from requests.exceptions import RequestException
+import requests
+import json
 
 
 
@@ -11,6 +14,7 @@ with open(論文名, 'r') as lunbun:
 content = [x.strip() for x in content]
 
 
+
 # 將文章照漢字台羅對應
 
 bracketregex = re.compile("\[")
@@ -18,8 +22,10 @@ han_lineregex = re.compile(".*?[，、。]")
 lo_lineregex = re.compile(".*?[,.]")
 
 for tsua in content:
+    if not tsua.strip():
+        continue
     # 才是漢羅對應文章
-    if '[' in tsua and ']' in tsua:
+    elif '[' in tsua and ']' in tsua:
         # get chapter
         try:
             han_ku, lo_ku = bracketregex.split(tsua)
@@ -41,4 +47,25 @@ for tsua in content:
             print('對齊失敗：{}\n{}\n'.format(han_ku, lo_ku))
 
     else:
-        print('揣無中括號：{}\n\n'.format(tsua))
+        # 補羅馬字
+        try:
+            r = requests.get('https://服務.意傳.台灣/標漢字音標', params={
+                '查詢腔口': '閩南語',
+                '查詢語句': tsua,
+            })
+        except RequestException as e:  # This is the correct syntax
+            print(e)
+            sys.exit(1)
+        
+        if r.status_code == requests.codes.ok:
+            pkg_str = r.content.decode('unicode_escape')
+            to_guan_arr = json.loads(pkg_str)
+            try:
+                peh_ue_ji = to_guan_arr['多元書寫'][0]['白話字'] 
+                print('{}\n[{}]\n\n'.format(tsua, peh_ue_ji.strip(' ')))
+            except IndexError:
+                print('tsua:{},,,{}'.format(tsua,to_guan_arr))
+                break
+        else:
+            print('回傳狀態毋是200：{}', tsua)
+        

30dbac5a5b7106388f6340d9f7acccb7a4c48fd5
a8568730 <a8568730@hotmail.com>
2018-09-07 10:57:42 +0800

    程式加解說。

--- lun5_bun5_thiah4_khui1.py
+++ lun5_bun5_thiah4_khui1.py
@@ -4,8 +4,11 @@ from requests.exceptions import RequestException
 import requests
 import json
 
+# 論文輸入：lun5_bun5/ （無入git）
+# 漢羅輸出：uan5_sing5/ （有上git）
 
 
+# 輸入論文
 論文名 = sys.argv[1]
 
 with open(論文名, 'r') as lunbun:
@@ -14,17 +17,16 @@ with open(論文名, 'r') as lunbun:
 content = [x.strip() for x in content]
 
 
-
-# 將文章照漢字台羅對應
-
+# 輸出論文每一句的漢羅對照
 bracketregex = re.compile("\[")
 han_lineregex = re.compile(".*?[，、。]")
 lo_lineregex = re.compile(".*?[,.]")
 
 for tsua in content:
+    # 空白tsua
     if not tsua.strip():
         continue
-    # 才是漢羅對應文章
+    # 有漢羅對應的論文段落
     elif '[' in tsua and ']' in tsua:
         # get chapter
         try:
@@ -39,33 +41,35 @@ for tsua in content:
         # get lines
         han_arr = han_lineregex.findall(han_ku)
         lo_arr = lo_lineregex.findall(lo_ku)
-        
+
         if len(lo_arr) == len(han_arr):
-            for a,b in zip(han_arr, lo_arr):
-                print('{}\n[{}]\n\n'.format(a,b.strip(' ')))
+            for a, b in zip(han_arr, lo_arr):
+                # 輸出每一句的漢羅
+                print('{}\n[{}]\n\n'.format(a, b.strip(' ')))
         else:
             print('對齊失敗：{}\n{}\n'.format(han_ku, lo_ku))
-
+    # 只有漢字的論文段落
     else:
-        # 補羅馬字
+        # 補段落的羅馬字
         try:
             r = requests.get('https://服務.意傳.台灣/標漢字音標', params={
                 '查詢腔口': '閩南語',
                 '查詢語句': tsua,
             })
-        except RequestException as e:  # This is the correct syntax
+        except RequestException as e:
             print(e)
             sys.exit(1)
-        
+
         if r.status_code == requests.codes.ok:
             pkg_str = r.content.decode('unicode_escape')
             to_guan_arr = json.loads(pkg_str)
             try:
-                peh_ue_ji = to_guan_arr['多元書寫'][0]['臺羅'] 
+                peh_ue_ji = to_guan_arr['多元書寫'][0]['臺羅']
+                # 輸出每一句的漢羅
                 print('{}\n[{}]\n\n'.format(tsua, peh_ue_ji.strip(' ')))
             except IndexError:
-                print('tsua:{},,,{}'.format(tsua,to_guan_arr))
+                # 無法度提著多元書寫
+                print('tsua:{},,,{}'.format(tsua, to_guan_arr))
                 break
         else:
             print('回傳狀態毋是200：{}', tsua)
-        
